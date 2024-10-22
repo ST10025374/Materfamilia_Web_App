61985062c1de1fab7177b370a796a2769df1ad48
Miguel Almeida <104522857+ST10025374@users.noreply.github.com>
2024-10-22 14:21:22 +0200

    Add project files.

---
+++ SampleApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,245 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp.Areas.Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly UserManager<IdentityUser> _userManager;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IUserStore<IdentityUser> _userStore;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IUserEmailStore<IdentityUser> _emailStore;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly ILogger<RegisterModel> _logger;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IEmailSender _emailSender;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly RoleManager<IdentityRole> _roleManager; //for Roles
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userManager"></param>
+        /// <param name="userStore"></param>
+        /// <param name="signInManager"></param>
+        /// <param name="logger"></param>
+        /// <param name="emailSender"></param>
+        /// <param name="roleManager"></param>
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            IUserStore<IdentityUser> userStore,
+            SignInManager<IdentityUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager) //For Roles
+        {
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+            _roleManager = roleManager; // for Roles
+        }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public string ReturnUrl { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public string Role { get; set; }
+
+        }
+
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// Handles the HTTP POST request for registering a new user.
+        /// </summary>
+        /// <param name="returnUrl">The URL to redirect to after successful registration.</param>
+        /// <returns>The IActionResult representing the result of the registration.</returns>
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = CreateUser();
+
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+
+                    // Assign role to the user
+                    string roleName = Input.Role; // Change this to the desired role
+
+                    // Check if the role exists
+                    if (!await _roleManager.RoleExistsAsync(roleName))
+                    {
+                        // Create the role if it doesn't exist
+                        await _roleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+
+                    // Assign the user to the role
+                    await _userManager.AddToRoleAsync(user, roleName);
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// Creates a new instance of the IdentityUser class.
+        /// </summary>
+        /// <returns>The newly created IdentityUser instance.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when an instance of IdentityUser cannot be created.</exception>
+        private IdentityUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<IdentityUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(IdentityUser)}'. " +
+                    $"Ensure that '{nameof(IdentityUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+
+        //---------------------------------------------------------------------//
+        /// <summary>
+        /// Gets the email store for the user.
+        /// </summary>
+        /// <returns>The email store for the user.</returns>
+        /// <exception cref="NotSupportedException">Thrown when the user manager does not support user email.</exception>
+        private IUserEmailStore<IdentityUser> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<IdentityUser>)_userStore;
+        }
+    }
+}
+//**------------------------------------------------------------< END >------------------------------------------------------------**// 
---
+++ SampleApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using SampleApp.Models;
+
+using System.Diagnostics;
+
+namespace SampleApp.Controllers
+{
+
+    //[Authorize(Roles = "Admin")]  //This is for the whole 
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize(Roles = "Admin")]// Only for privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
